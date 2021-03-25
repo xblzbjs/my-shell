fd794b0c68163d6f0774b77a1eaedb530ec2d500
xblzbjs <710255295@qq.com>
2021-03-25 17:24:14 +0800

    update

--- environment_setup.sh
+++ environment_setup.sh
@@ -4,7 +4,7 @@ function packageInstall(){
     if ! [ -x "$(command -v $1)" ];then
         echo "找不到$1"
         read -p "是否安装$1(y|n}):" -n 1 OK
-        if [ ${OK}="y" ] || [ ${OK} = "Y" ];then
+        if [ ${OK} = "y" ] || [ ${OK} = "Y" ];then
             echo
             echo"开始安装$1"
             brew install $1 || brew install --cask $1
@@ -13,6 +13,8 @@ function packageInstall(){
             else
                 echo "安装$1失败"
             fi
+        else
+            echo "不安装$1"
         fi
     else
         echo "$1已安装"
@@ -39,6 +41,10 @@ if [ $(uname) == "Darwin" ];then
         echo "homebrew已安装"
     fi
     # Git
-    packageInstall google-chrome
+    packageInstall postman
+    packageInstall docker
+    packageInstall go
+    packageInstall git
+    packageInstall visual-studio-code
 fi
 
